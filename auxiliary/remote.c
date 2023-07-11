49ee655aa70e1c1f1937b65c6c370edc9046d8ae
KseniyaShestakova <xxeniashestakova@gmail.com>
2023-07-11 14:54:08 +0000

    extending remote.c

--- auxiliary/remote.c
+++ auxiliary/remote.c
@@ -2,6 +2,7 @@
 #include "oio_core.h"
 
 #include <stdio.h>
+#include <string.h>
 
 static const char* ns = "OPENIO";
 static const char* account = "ACCOUNT";
@@ -32,7 +33,6 @@ void basic_init(struct oio_url_s* url, const char* name) {
 struct oio_url_s* create_empty_url() {
     struct oio_url_s* url = oio_url_empty();
     g_assert_nonnull(url);
-    printf("Created empty url...\n");
     return url;
 }
 
@@ -115,42 +115,42 @@ void metadata_example(struct oio_sds_s* client, struct oio_error_s* err) {
     fflush(stdout);
 }
 
-void listing_example(struct oio_sds_s* client, struct oio_error_s* err) {
+void fill_container(struct oio_sds_s* client, struct oio_error_s* err) {
     struct oio_url_s* url = create_empty_url();
-
     basic_init(url, NULL);
 
-    // ------------- fill container ---------------------
-    err = oio_sds_create(client, url);
+    err = oio_sds_create(client, url); // creating a container
     g_assert_no_error((GError*)err);
 
     struct oio_sds_ul_dst_s dst = OIO_SDS_UPLOAD_DST_INIT;
     dst.url = url;
-    gchar data[] = "sample";
+    gchar data[] = "Sample";
+    gchar foo[] = "foo/_";
+    size_t foo_pos = strlen(foo) - 1;
+    gchar object[] = "obj_";
+    size_t object_pos = strlen(object) - 1;
 
-    gchar* name = NULL;
-    for (int i = 0; i < 5; ++i) {
-        name = g_strdup_printf("object%d", i);
-        oio_url_set(url, OIOURL_PATH, name);
+    for (char i = 'a'; i < 'e'; ++i) {
+        foo[foo_pos] = i;
+        oio_url_set(url, OIOURL_PATH, foo);
         err = oio_sds_upload_from_buffer(client, &dst, data, sizeof(data));
-        g_free(name);
         g_assert_no_error((GError*)err);
     }
 
-    for (gchar id = 'a'; id <= 'd'; ++id) {
-        name = g_strdup_printf("foo/%c", id);
-        oio_url_set(url, OIOURL_PATH, name);
+    for (char i = '0'; i < '5'; ++i) {
+        object[object_pos] = i;
+        oio_url_set(url, OIOURL_PATH, object);
         err = oio_sds_upload_from_buffer(client, &dst, data, sizeof(data));
-        g_free(name);
         g_assert_no_error((GError*)err);
     }
 
     oio_url_clean(url);
-    //-------------------------------------
-    url = create_empty_url();
- 
-    basic_init(url, NULL);
+}
 
+// simply outputs the list of all objects in a container
+void basic_listing(struct oio_sds_s* client, struct oio_error_s* err) {
+    struct oio_url_s* url = create_empty_url();
+    basic_init(url, NULL);
 
     struct oio_sds_list_param_s list_in = {
         .url = url,
@@ -158,20 +158,191 @@ void listing_example(struct oio_sds_s* client, struct oio_error_s* err) {
         .flag_allversions = 1, .flag_nodeleted = 1, .flag_properties = 1
     };
 
-    // on_item, on_prefix, on_bound are functions, called at corresponding moments
+    int _print_item(void* ctx, const struct oio_sds_list_item_s* item) {
+        (void) ctx;
+        g_print("%s, ", item->name);
+        return 0;
+    }
+
     struct oio_sds_list_listener_s list_out = {
         .ctx = NULL,
-        .on_item = print_item, .on_prefix = NULL, .on_bound = NULL
+        .on_item = _print_item, .on_prefix = NULL, .on_bound = NULL
     };
 
-    printf("List of all objects:\n");
+    g_print("List of all items: ");
     err = oio_sds_list(client, &list_in, &list_out);
     oio_url_clean(url);
+    g_assert_no_error((GError*)err);
+    
+    g_print("END\n");
+    fflush(stdout);
+}
 
+// uses markers for outputing the list of all items in portions
+void marker_listing(struct oio_sds_s* client, struct oio_error_s* err) {
+    g_print("Example of listing with markers and max_item restriction was started...\n");
+    
+    struct oio_url_s* url = create_empty_url();
+    basic_init(url, NULL);
+
+    struct oio_sds_list_param_s list_in = {
+        .url = url,
+        .prefix = NULL, .marker = NULL, .end = NULL, .delimiter = 0, .max_items = 4,
+        .flag_allversions = 1, .flag_nodeleted = 1, .flag_properties = 1
+    };
+
+    gboolean save_marker;
+    gchar marker[16];
+    int _print_item(void* ctx, const struct oio_sds_list_item_s* item) {
+        (void) ctx;
+        g_print("%s, ", item->name);
+        if (save_marker) {
+            g_strlcpy(marker, item->name, 16);
+            save_marker = FALSE;
+        }
+        return 0;
+    }
+
+    struct oio_sds_list_listener_s list_out = {
+        .ctx = NULL,
+        .on_item = _print_item, .on_prefix = NULL, .on_bound = NULL
+    };
+
+    marker[0] = '\0';
+    do {
+        g_print("Objects: [");
+        save_marker = TRUE;
+        list_in.marker = marker;
+        err = oio_sds_list(client, &list_in, &list_out);
+        g_assert_no_error((GError*)err);
+        g_print("]\n");
+    } while (list_out.out_count);
+
+    oio_url_clean(url);
+}
+
+// output only objects starting with "foo"
+void prefix_listing(struct oio_sds_s* client, struct oio_error_s* err) {
+    g_print("Starting prefix example...\n");
+
+    struct oio_url_s* url = create_empty_url();
+    basic_init(url, NULL);
+
+    struct oio_sds_list_param_s list_in = {
+        .url = url,
+        .prefix = "foo", .marker = NULL, .end = NULL, .delimiter = 0, .max_items = 0,
+        .flag_allversions = 1, .flag_nodeleted = 1, .flag_properties = 1
+    };
+
+    int _print_item(void* ctx, const struct oio_sds_list_item_s* item) {
+        (void) ctx;
+        g_print("%s, ", item->name);
+        return 0;
+    }
+
+    struct oio_sds_list_listener_s list_out = {
+        .ctx = NULL,
+        .on_item = _print_item, .on_prefix = NULL, .on_bound = NULL
+    };
+
+    g_print("Objects with desired prefix: ");
+    err = oio_sds_list(client, &list_in, &list_out);
+    oio_url_clean(url);
+    g_assert_no_error((GError*)err);
+    g_print("END\n");
+}
+
+// excludes all the objects whose names contain delimiter
+void delimiter_listing(struct oio_sds_s* client, struct oio_error_s* err) {
+    g_print("Starting delimiter example...\n");
+
+    struct oio_url_s* url = create_empty_url();
+    basic_init(url, NULL);
+
+    struct oio_sds_list_param_s list_in = {
+        .url = url,
+        .prefix = NULL, .marker = NULL, .end = NULL, .delimiter = '/', .max_items = 0,
+        .flag_allversions = 1, .flag_nodeleted = 1, .flag_properties = 1
+    };
+
+    int _print_item(void* ctx, const struct oio_sds_list_item_s* item) {
+        (void) ctx;
+        g_print("%s, ", item->name);
+        return 0;
+    }
+
+    struct oio_sds_list_listener_s list_out = {
+        .ctx = NULL,
+        .on_item = _print_item, .on_prefix = NULL, .on_bound = NULL
+    };
+
+    g_print("Objects without delimeter: ");
+    err = oio_sds_list(client, &list_in, &list_out);
+    oio_url_clean(url);
+    g_assert_no_error((GError*)err);
+    g_print("END\n");
+}
+
+void listing_example(struct oio_sds_s* client, struct oio_error_s* err) {
+    fill_container(client, err);
+
+    basic_listing(client, err);
+
+    marker_listing(client, err);
+
+    prefix_listing(client, err);
+
+    delimiter_listing(client, err);
+
+    g_print("Passed listing example.\n\n");
+    fflush(stdout);
+}
+
+// in this case cleaning up means deleting current container
+// only empty container may be deleted
+// thus we need to delete objects inside the container first
+void clean_up(struct oio_sds_s* client, struct oio_error_s* err) {
+    g_print("Started cleaning up...\n");
+
+    struct oio_url_s* url = create_empty_url();
+    basic_init(url, NULL);
+
+    struct oio_sds_list_param_s list_in = {
+        .url = url,
+        .prefix = NULL, .marker = NULL, .end = NULL, .delimiter = 0, .max_items = 0,
+        .flag_allversions = 1, .flag_nodeleted = 1, .flag_properties = 1
+    };
+
+    int _delete_item(void* ctx, const struct oio_sds_list_item_s* item) {
+        (void) ctx;
+        g_print("Deleting item: %s...\n", item->name);
+
+        struct oio_url_s* tmp_url = create_empty_url();
+        basic_init(tmp_url, item->name);
+
+        err = oio_sds_delete(client, tmp_url);
+        oio_url_clean(tmp_url);
+        g_assert_no_error((GError*)err);
+    }
+
+    struct oio_sds_list_listener_s list_out = {
+        .ctx = NULL,
+        .on_item = _delete_item, .on_prefix = NULL, .on_bound = NULL
+    };
+
+    err = oio_sds_list(client, &list_in, &list_out);
+    oio_url_clean(url);
     g_assert_no_error((GError*)err);
 
-    printf("Passed listing example.\n\n");
+    g_print("Deleting container...");
+    url = create_empty_url();
+    basic_init(url, NULL);
 
+    err = oio_sds_delete_container(client, url);
+    g_assert_no_error((GError*)err);
+    oio_url_clean(url);
+
+    g_print("Cleaning up done.\n\n");
     fflush(stdout);
 }
 
@@ -191,11 +362,12 @@ void run_all_examples() {
 
     listing_example(client, err);
 
+    clean_up(client, err);
 }
 
 
 int main() {
     printf("Hello world!\n");
-    //run_all_examples();
+    run_all_examples();
  
 }
